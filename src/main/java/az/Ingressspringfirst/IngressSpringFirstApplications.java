82c8faacd4072f280a9cf37c55471820dcd1aa78
Fizaret <fizaretosmanov036@gmail.com>
2023-08-27 16:03:17 -0700

    .

---
+++ src/main/java/az/Ingressspringfirst/IngressSpringFirstApplications.java
@@ -0,0 +1,13 @@
+package az.Ingressspringfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  IngressSpringFirstApplications {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressSpringFirstApplications.class, args);
+    }
+
+}
---
+++ src/main/java/az/Ingressspringfirst/controller.java
@@ -0,0 +1,4 @@
+package az.Ingressspringfirst;
+
+public class controller {
+}
